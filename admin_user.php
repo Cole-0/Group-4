ed7f9aa15e3a6ee4e01aa3f9898871ac98f88085
fakrarrXD <ninochristophersanjoseschool@gmail.com>
2024-04-12 23:02:10 +0800

    forgot to change

--- admin_user.php
+++ admin_user.php
@@ -25,11 +25,8 @@ if (isset($_POST['submit'])) {
 <html lang="en">
 
 <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>ADMIN ADD</title>
 </head>
