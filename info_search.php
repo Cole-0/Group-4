0ba7abb7cd3d52d4c406b13bbd040902374b9775
Wilmer Navarro <165784587+Wilmerrrrr@users.noreply.github.com>
2024-04-28 10:37:58 +0800

    in progress

--- info_search.php
+++ info_search.php
@@ -5,17 +5,17 @@ require 'db/connect.php';
 if (isset($_POST['searchText']) && !empty($_POST['searchText'])) {
     // Sanitize the input to prevent SQL injection
     $searchText = htmlspecialchars($_POST['searchText']);
-    
+
     // Prepare the SQL query to search for names based on the provided search text
     $searchSql = "SELECT tbl_users.fname, tbl_users.lname
                   FROM tbl_users
                   WHERE tbl_users.fname LIKE :searchText
                   OR tbl_users.lname LIKE :searchText";
-                  
+
     $stmtSearch = $conn->prepare($searchSql);
     $stmtSearch->bindValue(':searchText', '%' . $searchText . '%', PDO::PARAM_STR);
     $stmtSearch->execute();
-    
+
     $searchResult = $stmtSearch->fetchAll(PDO::FETCH_ASSOC);
 
     if (count($searchResult) > 0) {
@@ -30,19 +30,19 @@ if (isset($_POST['searchText']) && !empty($_POST['searchText'])) {
             $counter++;
         }
         // Add a button to go back to the main table
-        echo "<tr><td colspan='4'><button id='backToMainTable' class='btn btn-secondary'>Back to Main Table</button></td></tr>";
+        echo "<tr><td colspan='4'><button id='backToMainTable' class='btn btn-secondary'>Back</button></td></tr>";
     } else {
         echo "<tr><td colspan='4'>No matching names found.</td></tr>";
-        echo "<tr><td colspan='4'><button id='backToMainTable' class='btn btn-secondary'>Back to Main Table</button></td></tr>";
+        echo "<tr><td colspan='4'><button id='backToMainTable' class='btn btn-secondary'>Back</button></td></tr>";
     }
 } else {
     // Original query to fetch full main table data
     $originalSql = "SELECT tbl_users.fname, tbl_users.lname
                     FROM tbl_users";
-                  
+
     $stmtOriginal = $conn->prepare($originalSql);
     $stmtOriginal->execute();
-    
+
     $originalResult = $stmtOriginal->fetchAll(PDO::FETCH_ASSOC);
 
     if (count($originalResult) > 0) {
@@ -60,4 +60,3 @@ if (isset($_POST['searchText']) && !empty($_POST['searchText'])) {
         echo "<tr><td colspan='4'>No names found.</td></tr>";
     }
 }
-?>
